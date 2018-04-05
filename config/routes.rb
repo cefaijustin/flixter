3e501dce919ec6943aabd1d15956d2efdfc007fb
Justin Cefai <you@example.com>
2018-04-05 15:37:23 +0000

    next lesson button

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   resource :dashboard, only: [:show]
  	root 'static_pages#index'
+
  	resources :courses, only: [:index, :show] do
  		resources :enrollments, only: :create
  	end	
+
  	resources :lessons, only: [:show]
+
  	namespace :instructor do
  		resources :lessons, only: [:update]
- 		resources :sections, only: [:update] do
+ 			resources :sections, only: [:update] do
+ 	resources :lessons, only: [:create]			
  		end	
  		resources :courses, only: [:new, :create, :show] do
  			resources :sections, only: [:create]
